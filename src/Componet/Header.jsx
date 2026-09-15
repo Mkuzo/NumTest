b030c6c327a53f9a5af4d9d374125250a413658b
Lohay <lohayvalentino@gmail.com>
2026-09-15 16:50:48 +0300

    code

---
+++ src/Componet/Header.jsx
@@ -0,0 +1,15 @@
+export default function Header(){
+    return(
+        <div>
+            <div className="header-container">
+                <div className="header">
+                    <ul>
+                        <li>Home</li>
+                        <li>About Us</li>
+                        <li>Contact Us</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
